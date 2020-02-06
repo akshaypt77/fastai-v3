c65a2f3383b9613e6a2ea9e8cf0c78fa8ed31ee4
akshaypt77 <58225170+akshaypt77@users.noreply.github.com>
2020-02-06 09:29:15 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1q3peprRKi1fFXHFTVXbSSuI_UFUSnY5i'
+export_file_url = 'https://drive.google.com/uc?export=download&id=11988uOFuEFPUF1RTP6QdjNnIK_TGNYzZ'
 export_file_name = 'export.pkl'
 
 classes = ['puttu', 'pathiri', 'poratta']
